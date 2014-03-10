5500c2727cb0f9a56d9eb2b0fd096b76c4e06a4c
mutexre <alexander.obuschenko@gmail.com>
2014-03-10 19:50:22 +0800

    add UI

---
+++ Sources/UI/Input.h
@@ -0,0 +1,27 @@
+#ifndef header_722238C8_E600_4F4A_889C_7E588DF589A9
+#define header_722238C8_E600_4F4A_889C_7E588DF589A9
+
+namespace UI
+{
+    enum Key {
+        space,
+        esc,
+        enter,
+        backspace,
+        up, down, left, right,
+        pageUp, pageDown,
+        home, end,
+        f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12
+    };
+
+    enum Modifier {
+        shift,
+        alt,
+        ctrl,
+        cmd
+    };
+    
+    void convertKeyCode(Rt::Option<Key>, Rt::Option<std::string>);
+}
+
+#endif
---
+++ Sources/UI/UI.h
@@ -0,0 +1,6 @@
+#ifndef header_E98028FD653E
+#define header_E98028FD653E
+
+#include <UI/Input.h>
+
+#endif
